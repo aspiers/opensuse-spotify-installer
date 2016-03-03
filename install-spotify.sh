627a8e44f429bc92117b1ef05de77be4c24b5e71
CornGuo <cornguo@msn.com>
2016-03-03 14:43:28 +0800

    safe_run

--- install-spotify.sh
+++ install-spotify.sh
@@ -78,7 +78,8 @@ main () {
         if check_not_installed; then
             echo "Creating spec file from template..."
             SPEC_TEMPLATE="$RPM_SPEC_DIR/${RPM_NAME}.spec"
-            cat $SPEC_TEMPLATE | sed "s/VERTOKEN/$VERSION/g" | sed "s/DEB_AMD64/$FILE_AMD64/g" | sed "s/DEB_I386/$FILE_I386/g" > /tmp/$RPM_NAME.spec
+            safe_run cat $SPEC_TEMPLATE | sed "s/VERTOKEN/$VERSION/g" | sed "s/DEB_AMD64/$FILE_AMD64/g" | sed "s/DEB_I386/$FILE_I386/g" > /tmp/$RPM_NAME.spec
+
             safe_run mkdir -p "$RPM_TOPDIR"/{BUILD,BUILDROOT,SPECS,SOURCES,SRPMS,RPMS/{i586,x86_64}}
             install_rpm_build
             echo
